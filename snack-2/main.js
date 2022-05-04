4a1aa70f3668c85d280b7734c7589890d8ea6fc1
Giaki22 <giaki22@hotmail.com>
2022-05-04 11:58:55 +0200

    + Fix Snack-2

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,10 +4,10 @@ a = aa.length;
 b = bb.length;
 
 if (a>b){
-    alert(aa,bb);
+    alert(bb,aa);
 } 
 else if (b>a){
-    alert(bb,aa);
+    alert(aa,bb);
 }
 else{
     alert("Le parole hanno la stessa lunghezza");

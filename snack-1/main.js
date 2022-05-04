97a0e92900052cdadeeb2da67705295912391ae8
Giaki22 <giaki22@hotmail.com>
2022-05-04 11:29:02 +0200

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,12 @@
+let a = prompt("Inserisci un numero");
+let b = prompt("Inserisci un numero");
+
+if (a>b){
+    alert(a);
+} 
+else if (b>a){
+    alert(b);
+}
+else{
+    alert("I numeri sono uguali");
+}
